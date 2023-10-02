35c4d310e8ad0c8c7e7aa55fbf6add06e2f0a174
jhosebro <jhosebro2108@gmail.com>
2023-10-02 03:02:02 -0500

    Primer Commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const secretKey = crypto.randomBytes(32).toString("hex");
+
+function authenticationUser(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Acceso no autorizado. Inicia Sesión para continuar" });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ message: "Token invalido. Inicia sesión para continuar" });
+    }
+
+    req.user = decoded;
+    next();
+  });
+}
+
+const authController = require("./controllers/authController");
+
+app.post("/api/auth/login", authController.login);
+app.post("/api/auth/register", authController.registerUser);
+
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
---
+++ controllers/authController.js
@@ -0,0 +1,87 @@
+const mysql = require("mysql2");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const db = mysql.createConnection({
+  host: "db4free.net",
+  user: "jhosebro",
+  password: "Gogeta123,",
+  database: "medtrack",
+});
+
+const login = (req, res) => {
+  const { username, password } = req.body;
+
+  const query = "SELECT * FROM User WHERE username = ?";
+
+  db.query(query, [username], (err, result) => {
+    if (err) {
+      console.error("Error al buscar el usuario:", err);
+      res.status(500).json({ error: "Error al buscar el usuario" });
+      return; // Importante agregar return para salir de la función
+    }
+
+    if (result.length === 0) {
+      res.status(401).json({ error: "Usuario no encontrado" });
+      return;
+    }
+
+    const usuarioEncontrado = result[0];
+
+    bcrypt.compare(password, usuarioEncontrado.password, (error, match) => {
+      if (error) {
+        console.error("Error al comparar contraseña:", error);
+        res.status(500).json({ error: "Error de autenticación" });
+        return;
+      }
+
+      if (match) {
+        res.json({ mensaje: "Inicio de sesión exitoso " });
+      } else {
+        res.status(401).json({ error: "Contraseña incorrecta" });
+      }
+    });
+  });
+};
+
+
+const registerUser = (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: "Por favor, completa todos los campos." });
+  }
+
+  db.query("SELECT * FROM User WHERE email = ? OR username = ?", [email, username], (err, existingUser) => {
+      if(err){
+        console.error("Error al buscar el usuario: ", err);
+        return res.status({ error: "Error al buscar el usuario"});
+      }
+
+      if(existingUser.length > 0){
+        return res.status(400).json({
+          message: "El correo electrónico o el usuario ya se encuentra registrado"});
+      }
+
+      bcrypt.hash(password, saltRounds, (error, hashedPassword) => {
+        if(error){
+          console.error("Error al generar el hash de la contraseña: ", error);
+          return res.status(500).json({ error: "Error al crear el usuario"});
+        }
+
+        db.query("INSERT INTO User (username, email, password) VALUES (?, ?, ?)",[username, email, hashedPassword], (insertErr) => {
+          if(insertErr){
+            console.error("Error al insertar el usuario en la BD", insertErr);
+            return res.status(500).json({ error: "Error interno del servidor"});
+          }
+          return res.status(201).json({ message: "Usuario creado exitosamente" })
+      });
+    });
+  });
+}
+
+
+module.exports = {
+  login,
+  registerUser,
+};
