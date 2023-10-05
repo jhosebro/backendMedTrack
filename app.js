5db7de4a0dbb19972b00f683f02e212239719c50
jhosebro <jhosebro2108@gmail.com>
2023-10-05 17:34:04 -0500

    Hago revert

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json(), cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
 const secretKey = crypto.randomBytes(32).toString("hex");
 
